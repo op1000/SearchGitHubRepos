823c6c248414c2fd15ece7278bdf686dbbd4cae5
NakcheonJung <ncjung1000@gmail.com>
2023-03-07 15:50:33 +0900

    SGR-005 Implement SearchResultsView.

--- SearchGitHubRepos/Search/UI/SearchResultsView.swift
+++ SearchGitHubRepos/Search/UI/SearchResultsView.swift
@@ -5,4 +5,42 @@
 //  Created by NakCheon Jung on 2023/03/07.
 //
 
-import Foundation
+import SwiftUI
+
+struct SearchResultsView: View {
+    @ObservedObject private var viewModel: SearchViewModel
+    
+    init(viewModel: SearchViewModel = SearchViewModel()) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        SearchBar(viewModel: viewModel)
+            .padding(20)
+        
+        List(viewModel.searchResults) { item in
+            ResultCell(repo: item, viewModel: viewModel)
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ResultCell: View {
+    @ObservedObject private var viewModel: SearchViewModel
+    @State private var repo: SearchViewModel.Repo
+    
+    init(repo: SearchViewModel.Repo, viewModel: SearchViewModel) {
+        self.repo = repo
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(repo.name)
+                .font(.system(size: 20, weight: .bold))
+            Text(repo.owenr)
+                .font(.system(size: 16, weight: .regular))
+        }
+    }
+}
+
