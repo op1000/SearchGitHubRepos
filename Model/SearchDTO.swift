9cd1586c3b5d0ec08ddebe4b1a6aa5113a5fb4bd
NakcheonJung <ncjung1000@gmail.com>
2023-03-07 15:03:25 +0900

    SGR-003 Implement SearchDTO.swift
    Add Response DTOs

--- Model/SearchDTO.swift
+++ Model/SearchDTO.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct SearchRepositoryDTO: Decodable {
+    let total_count: Int
+    let incomplete_results: Bool
+    let items: [RepoInfo]
+}
+
+struct RepoInfo: Decodable {
+    let id: Int
+    let name: String
+    let full_name: String
+    let html_url: String
+    let description: String?
+    let owner: User
+}
+
+struct User: Decodable {
+    let id: Int
+    let login: String
+}
