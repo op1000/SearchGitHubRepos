a9c0730be255915660eae85ca9f48963c0d12993
NakcheonJung <ncjung1000@gmail.com>
2023-03-07 15:52:00 +0900

    SGR-005 connect main view.

--- SearchGitHubRepos/SearchGitHubReposApp.swift
+++ SearchGitHubRepos/SearchGitHubReposApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SearchGitHubReposApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SearchResultsView()
         }
     }
 }
