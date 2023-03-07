f57aae5ed7ceaf9f114bc49e61c7d6c64ff515bf
NakcheonJung <ncjung1000@gmail.com>
2023-03-07 15:29:42 +0900

    SGR-003 implement GitHubSearchAPI as Data layer.

--- Data/GitHubSearchAPI.swift
+++ Data/GitHubSearchAPI.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Combine
+import Alamofire
+
+class GitHubSearchAPIs {
+    private enum Constants {
+        static let baseURL = "https://api.github.com"
+        static let searchRepositories = "/search/repositories"
+        static let defaultHeaders: HTTPHeaders = [
+            "accept" : "application/vnd.github.v3+json"
+        ]
+    }
+    
+    func searchRepositories(query: String) -> AnyPublisher<DataResponse<SearchRepositoryDTO, Error>?, Never> {
+        let stringURL = Constants.baseURL + Constants.searchRepositories + "?q=" + query
+        guard let encodedStringURL = stringURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return Just(nil).eraseToAnyPublisher()
+        }
+        
+        return AF.request(encodedStringURL, method: .get, parameters: ["q": query], headers: Constants.defaultHeaders)
+            .publishDecodable(type: SearchRepositoryDTO.self)
+            .map { response in
+                response.mapError { error in
+                    return NetworkError(initialError: error)
+                }
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }    
+}
