8902e66549a602b5cbe0d540d433985eb7f8e37d
NakcheonJung <ncjung1000@gmail.com>
2023-03-07 14:42:55 +0900

    SGR-001 Project initial commit.

---
+++ SearchGitHubRepos/SearchGitHubReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchGitHubReposApp.swift
+//  SearchGitHubRepos
+//
+//  Created by NakCheon Jung on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SearchGitHubReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
